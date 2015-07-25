343dc480bebb5dcfbe43aca4cefae2455f92ddfa
BRIAN ATWOOD <batwood011@gmail.com>
2015-07-24 20:34:58 -0500

    adds https GET request to riot server

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,33 @@
 var express = require('express');
+var https = require('https');
 var router = express.Router();
 
-/* GET home page. */
+var key = '2d13bb23-9bde-4668-82a0-2c0615eb6a92'
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'StatsGetter' });
 });
 
-// change this from stats
+
 router.get('/stats', function(req, res, next) {
-  res.end('stats!');
+
+  https.get('https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/RiotSchmick?api_key=' + key, function(response, err) {
+    
+    if (err) { console.log(err) }
+
+    var data = '';
+
+    response.on('data', function(stream){
+      data += stream
+    })
+
+    response.on('end', function(){
+      res.end(data);
+    })
+    
+  })
+
+
 })
 
 module.exports = router;
